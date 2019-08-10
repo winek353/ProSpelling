aa82c2ef426519d88d2847e7d21f9a21572c786a
Dawid <winiu31@gmail.com>
2019-08-10 21:39:49 +0200

    added create flashcard layout and adjusted mainActivity

---
+++ app/src/main/java/com/example/proSpelling/CreateFlashcardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.proSpelling
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateFlashcardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_flashcard)
+    }
+}
--- app/src/main/java/com/example/proSpelling/MainActivity.kt
+++ app/src/main/java/com/example/proSpelling/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.proSpelling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCreate.setOnClickListener{
+            startActivity(Intent(this, CreateFlashcardActivity::class.java))
+        }
     }
 }
